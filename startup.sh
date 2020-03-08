c84a296d7c022a6529eafd0548affd6fb2365a1c
liusen <331505785@qq.com>
2020-03-08 11:02:39 +0800

    change file name

--- startup.sh
+++ startup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+# (1) Variable
 FQCN="lsieun.EMail"
 DIR_PATH="$(cd "$(dirname "$0")"; pwd -P)"
 CLASS_PATH="${DIR_PATH}/target/classes"
+LIB_PATH="${DIR_PATH}/target/lib"
 
+# (2) Compile
 cd ${DIR_PATH}
 mvn clean compile
 
+# (3) copy Resource
 function copy_resource {
   local from_path="${DIR_PATH}/${1}"
   local to_path="${CLASS_PATH}/${1}"
@@ -19,6 +23,17 @@ function copy_resource {
 copy_resource config.properties
 copy_resource logging.properties
 
+# (4) collect JAR PATH
+JAR_PATH=""
+cd ${LIB_PATH}
+for jar_file in *.jar
+do
+    JAR_PATH="${LIB_PATH}/${jar_file}:"
+done
 
-nohup java -cp "${CLASS_PATH}" "${FQCN}" > /dev/null 2>&1 &
+echo "${JAR_PATH}${CLASS_PATH}"
+
+# (5) Run Program
+cd ${DIR_PATH}
+nohup java -cp "${JAR_PATH}${CLASS_PATH}" "${FQCN}" > /dev/null 2>&1 &
 
